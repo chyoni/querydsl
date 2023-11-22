cd16a8491b81866f78245215c9d3330e33fd4aa3
chyonee <chiwon99881@gmail.com>
2023-11-22 13:47:21 +0900

    search, search condition, sorting

--- src/test/java/com/example/querydsl/QueryDslBasicTest.java
+++ src/test/java/com/example/querydsl/QueryDslBasicTest.java
@@ -2,6 +2,7 @@ package com.example.querydsl;
 
 import com.example.querydsl.entity.Member;
 import com.example.querydsl.entity.Team;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static com.example.querydsl.entity.QMember.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -64,5 +67,60 @@ public class QueryDslBasicTest {
         assert findMember != null;
 
         assertEquals(findMember.getUsername(), "member1");
+    }
+
+    @Test
+    public void search() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1").and(member.age.eq(10)))
+                .fetchOne();
+
+        assert findMember != null;
+        assertEquals(findMember.getUsername(), "member1");
+    }
+
+    @Test
+    public void searchNoAnd() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"),
+                        member.age.eq(10)
+                )
+                .fetchOne();
+
+        assert findMember != null;
+        assertEquals(findMember.getUsername(), "member1");
+    }
+
+    @Test
+    public void resultFetch() {
+        List<Member> fetch = queryFactory.selectFrom(member).fetch();
+
+        Member fetchOne = queryFactory.selectFrom(member).fetchOne();
+
+        Member fetchFirst = queryFactory.selectFrom(member).fetchFirst();
+
+    }
+
+    @Test
+    public void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member1", 100));
+        em.persist(new Member("member2", 100));
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member1 = result.get(0);
+        Member member2 = result.get(1);
+        Member member3 = result.get(2);
+
+        assertEquals(member1.getUsername(), "member1");
+        assertEquals(member2.getUsername(), "member2");
+        assertNull(member3.getUsername());
     }
 }
