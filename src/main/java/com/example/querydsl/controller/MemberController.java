2f255d285c74cde5e4d81ff18de9e9fa34e2ee3f
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2023-11-26 19:30:51 +0900

    spring-data-jpa paging + querydsl 2

--- src/main/java/com/example/querydsl/controller/MemberController.java
+++ src/main/java/com/example/querydsl/controller/MemberController.java
@@ -3,8 +3,11 @@ package com.example.querydsl.controller;
 import com.example.querydsl.dto.MemberSearchCondition;
 import com.example.querydsl.dto.MemberTeamDto;
 import com.example.querydsl.repository.MemberJpaRepository;
+import com.example.querydsl.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +19,7 @@ import java.util.List;
 public class MemberController {
 
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * Parameter에 클래스를 받으면 이게 쿼리 파라미터에 존재하는 key/value
@@ -24,5 +28,10 @@ public class MemberController {
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.search(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPaging(condition, pageable);
     }
 }
--- src/main/java/com/example/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/com/example/querydsl/repository/MemberRepositoryImpl.java
@@ -3,12 +3,15 @@ package com.example.querydsl.repository;
 import com.example.querydsl.dto.MemberSearchCondition;
 import com.example.querydsl.dto.MemberTeamDto;
 import com.example.querydsl.dto.QMemberTeamDto;
+import com.example.querydsl.entity.Member;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
@@ -59,7 +62,7 @@ public class MemberRepositoryImpl implements MemberRepositoryQueryDsl {
                 .limit(pageable.getPageSize())
                 .fetch();
 
-        int total = queryFactory
+        /*int total = queryFactory
                 .select(member)
                 .from(member)
                 .leftJoin(member.team, team)
@@ -69,9 +72,22 @@ public class MemberRepositoryImpl implements MemberRepositoryQueryDsl {
                         ageLoe(condition.getAgeLoe())
                 )
                 .fetch()
-                .size();
+                .size();*/
 
-        return new PageImpl<>(result, pageable, total);
+        // return new PageImpl<>(result, pageable, total);
+
+        JPAQuery<Member> countQuery = queryFactory
+                .select(member)
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()));
+
+        // 만약, 전체 데이터가 100개인데 불러오는 데이터가 100개 이상이면 토탈 카운트를 가져올 필요가 없겠지? 그럴 때 !
+        // 이렇게 PageableExecutionUtils.getPage()의 3번째 인자로 저렇게 JPAQuery 타입으로 주면 필요없는 경우 쿼리를 안 날린다. 최고다.
+        return PageableExecutionUtils.getPage(result, pageable, () -> countQuery.stream().count());
     }
 
     private BooleanExpression ageLoe(Integer ageLoe) {
